fe6e52f9b6a696adc6ac67b0a7d5fade3e24a640
ZKSCSharp <55925334+ZKSCSharp@users.noreply.github.com>
2022-12-11 19:29:48 +0100

    1st test publish npm

--- Discord/Discord.js
+++ Discord/Discord.js
@@ -1,7 +1,9 @@
 const { Client, GatewayIntentBits } = require("discord.js")
+const { CommandsBuilder } = require('../Utils/CommandsBuilder')
 
 exports.DiscordJS = {
     newClient : function() { return new Client( {intents: [GatewayIntentBits.Guilds]} ); },
+    newCommands : (name) => CommandsBuilder.newCommand(name),
     commands : [],
     prefix : "!",
     databases : [], //Add db table as array here
--- package.js
+++
@@ -1,8 +0,0 @@
-const { DiscordJS } = require('./Discord/Discord');
-
-const client = DiscordJS.newClient();
-
-client.on('ready', () => {
-    console.log("ok")
-});
-test.login("DISCORD_TOKEN_BOT");
